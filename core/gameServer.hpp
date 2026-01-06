bec532eece1de5644be37495afd608c208ca9d34
PiotrTrybala <trybalapiotr4@gmail.com>
2026-01-06 20:02:26 +0100

    implemented header of game server

--- core/gameServer.hpp
+++ core/gameServer.hpp
@@ -12,35 +12,34 @@
 using asio::ip::udp;
 using namespace tag::lib::network;
 
-class GameServer
-{
-public:
-    GameServer(uint16_t port);
-    ~GameServer();
+class GameServer {
+    public:
+        GameServer(uint16_t port);
+        ~GameServer();
 
-    void Run();
+        void Run();
 
-    void Broadcast(const Packet& packet);   
+        void Broadcast(const Packet& packet);
 
-    void Send(const Packet& packet, const udp::endpoint& endpoint);
-    void SendClient(const Packet& packet, const Client& client);
+        void Send(const udp::endpoint& endpoint, const Packet& packet);
+    private:
+        void startReceive();
+        void handleReceive(Packet& packet);
 
-private:
-    void StartReceive();
-    void HandleReceive(Packet& packet);
+        asio::io_context ioContext;
+        asio::executor_work_guard<asio::io_context::executor_type> workGuard;
+        udp::socket socket;
+        udp::endpoint endpoint;
 
-    asio::io_context ioContext;
-    asio::executor_work_guard<asio::io_context::executor_type> workGuard;
-    udp::socket socket;
-    udp::endpoint remoteEndpoint;
-    uint8_t recvBuffer[2048];
+        uint8_t recvBuffer[2048];
 
-    std::thread serverThread;
+        std::thread serverThread;
 
-    std::map<uint32_t, Client> clients;
+        std::mutex clientsMutex;
+        std::map<uint32_t, Client> clients;
 
-    bool running = false;
-    uint32_t nextId = 1;
+        bool running = false;
+        uint32_t nextId = 1;
 
-    const double tickRate = 1.0 / 60.0;
+        const double tickRate = 1.0 / 60.0;
 };
