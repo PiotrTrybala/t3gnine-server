771ff846c2ea59339d400ab5b8023a235167c766
PiotrTrybala <trybalapiotr4@gmail.com>
2026-01-09 20:44:21 +0100

    implemented server player header

--- core/serverPlayer.hpp
+++ core/serverPlayer.hpp
@@ -1,6 +1,38 @@
 #pragma once
 
-class ServerPlayer {
-    public:
-    private:
+#include <btBulletDynamicsCommon.h>
+#include <BulletDynamics/Character/btKinematicCharacterController.h>
+
+struct ServerInput
+{
+    int direction;
+    bool jumping;
+    double timestamp;
+};
+
+class ServerPlayer
+{
+public:
+    ServerPlayer(uint32_t id, btPairCachingGhostObject *object, btKinematicCharacterController *controller);
+    ~ServerPlayer();
+
+    uint32_t GetID() const;
+    btKinematicCharacterController *GetController() const;
+
+    void ApplyInput(const ServerInput &input);
+
+    void PostPhysicsUpdate(float deltaTime);
+
+    btVector3 GetPosition() const;
+
+private:
+    uint32_t id;
+
+    btPairCachingGhostObject* ghost;
+    btKinematicCharacterController* controller;
+
+    btVector3 pendingWalkDir;
+    bool pendingJump = false;
+
+    ServerInput lastInput;
 };
