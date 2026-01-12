aa623ab45703aa0fa15693a739ca2a3e04266933
PiotrTrybala <trybalapiotr4@gmail.com>
2026-01-12 17:34:43 +0100

    implementing scene class

---
+++ scene.cpp
@@ -0,0 +1,62 @@
+#include "scene.hpp"
+
+Scene::Scene()
+{
+    collisionConfiguration = new btDefaultCollisionConfiguration();
+    dispatcher = new btCollisionDispatcher(collisionConfiguration);
+    broadphase = new btDbvtBroadphase();
+    solver = new btSequentialImpulseConstraintSolver();
+    dynamicsWorld = new btDiscreteDynamicsWorld(dispatcher, broadphase, solver, collisionConfiguration);
+
+    dynamicsWorld->setGravity(btVector3(0, -9.81f, 0));
+}
+Scene::~Scene()
+{
+    for (auto &[id, body] : rigidBodies)
+        dynamicsWorld->removeRigidBody(body);
+
+    rigidBodies.clear();
+    colliders.clear();
+    players.clear();
+
+    delete dynamicsWorld;
+    delete solver;
+    delete broadphase;
+    delete dispatcher;
+    delete collisionConfiguration;
+}
+
+void Scene::AddCollider(const btVector3& position, const btVector3& scale)
+{
+}
+void Scene::RemoveCollider(uint32_t id)
+{
+}
+
+void Scene::AddRigidBody(btRigidBody *body)
+{
+}
+void Scene::RemoveRigidBody(uint32_t id)
+{
+}
+
+void Scene::AddPlayer(Player *player)
+{
+    players[player->GetID()] = player;
+}
+void Scene::RemovePlayer(uint32_t id)
+{
+    auto it = players.find(id);
+    if (it == players.end()) return;
+
+    players.erase(id);
+}
+
+void Scene::Update(float deltaTime)
+{
+
+
+
+
+    
+}
--- scene.hpp
+++ scene.hpp
@@ -11,7 +11,20 @@ public:
     Scene();
     ~Scene();
 
+    void AddCollider(const btVector3& position, const btVector3& scale);
+    void RemoveCollider(uint32_t id);
+
+    void AddRigidBody(btRigidBody* body);
+    void RemoveRigidBody(uint32_t id);
+
+    void AddPlayer(Player* player);
+    void RemovePlayer(uint32_t id);
+
+    void Update(float deltaTime);
+
 private:
+    std::unordered_map<uint32_t, btCollisionObject *> colliders;
+    std::unordered_map<uint32_t, btRigidBody *> rigidBodies;
     std::unordered_map<uint32_t, Player *> players;
 
     btDefaultCollisionConfiguration *collisionConfiguration;
