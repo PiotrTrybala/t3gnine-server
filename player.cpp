d0557b85ac757e2d0da7b5a3136be8699e5bed63
PiotrTrybala <trybalapiotr4@gmail.com>
2026-01-15 21:42:08 +0100

    implemented player class

--- player.cpp
+++ player.cpp
@@ -1,80 +1,86 @@
-// #include "player.hpp"
+#include "player.hpp"
 
-// Player::Player(uint32_t id) : id(id)
-// {
-//     btConvexShape *shape = new btCapsuleShape(DEFAULT_CAPSULE_PLAYER_SHAPE.x, DEFAULT_CAPSULE_PLAYER_SHAPE.y);
-//     ghostObject = new btPairCachingGhostObject();
+Player::Player(uint32_t id) : id(id)
+{
+    btConvexShape *shape = new btCapsuleShape(DEFAULT_CAPSULE_PLAYER_SHAPE.x, DEFAULT_CAPSULE_PLAYER_SHAPE.y);
+    ghostObject = new btPairCachingGhostObject();
 
-//     btTransform trans;
-//     trans.setOrigin(btVector3(0, 0, 0));
-//     // TODO: Set rotation later
+    btTransform trans;
+    trans.setOrigin(btVector3(0, 0, 0));
 
-//     ghostObject->setWorldTransform(trans);
-//     ghostObject->setCollisionShape(shape);
-//     ghostObject->setCollisionFlags(btCollisionObject::CF_CHARACTER_OBJECT);
-//     ghostObject->setUserPointer(this);
+    ghostObject->setWorldTransform(trans);
+    ghostObject->setCollisionShape(shape);
+    ghostObject->setCollisionFlags(btCollisionObject::CF_CHARACTER_OBJECT);
+    ghostObject->setUserPointer(this);
 
-//     controller = new btKinematicCharacterController(ghostObject, shape, 0.25f);
+    controller = new btKinematicCharacterController(ghostObject, shape, 0.25f);
 
-//     lastInput = {
-//         .id = 0,
-//         .sequence = 0,
-//         .front = glm::vec3(0.0f, 0.0f, 0.0f),
-//         .directionMask = 0,
-//         .isJumping = false,
-//     };
-// }
-// Player::~Player()
-// {
-//     delete ghostObject;
-//     delete controller;
-// }
+    lastInput = {0, 0, glm::vec3(0.0f), 0, false};
+    lastSequence = 0;
+}
+Player::~Player()
+{
+    delete controller;
+}
 
-// const uint32_t Player::GetID()
-// {
-//     return this->id;
-// }
-// const btVector3 &Player::GetPosition()
-// {
-//     return ghostObject->getWorldTransform().getOrigin();
-// }
+const uint32_t Player::GetID()
+{
+    return id;
+}
+const uint32_t Player::GetLastSequence()
+{
+    return lastSequence;
+}
+const btVector3 &Player::GetPosition()
+{
+    return ghostObject->getWorldTransform().getOrigin();
+}
+void Player::SetPosition(const btVector3 &position)
+{
+    btTransform trans;
+    trans.setOrigin(position);
 
-// void Player::ApplyInput(const PlayerInput &input)
-// {
-//     lastInput = input;
+    ghostObject->setWorldTransform(trans);
+}
 
-//     glm::vec3 up(0.0f, 1.0f, 0.0f);
-//     glm::vec3 right = glm::normalize(glm::cross(up, input.front));
+void Player::ApplyInput(const PlayerInput &input)
+{
+    lastInput = input;
+    lastSequence = input.sequence;
 
-//     glm::vec3 movementDirection(0.0f);
+    glm::vec3 up(0.0f, 1.0f, 0.0f);
+    glm::vec3 right = glm::normalize(glm::cross(up, input.direction));
 
-//     if (input.directionMask & WalkDirection::FRONT)
-//         movementDirection += input.front;
-//     if (input.directionMask & WalkDirection::BACK)
-//         movementDirection -= input.front;
-//     if (input.directionMask & WalkDirection::RIGHT)
-//         movementDirection += right;
-//     if (input.directionMask & WalkDirection::LEFT)
-//         movementDirection -= right;
+    glm::vec3 walkDirection(0.0f);
 
-//     if (glm::length(movementDirection) > 0.001f)
-//     {
-//         movementDirection = glm::normalize(movementDirection);
-//     }
+    if (input.directionMask & WalkDirection::FRONT)
+        walkDirection += input.direction;
+    if (input.directionMask & WalkDirection::BACK)
+        walkDirection -= input.direction;
+    if (input.directionMask & WalkDirection::RIGHT)
+        walkDirection += right;
+    if (input.directionMask & WalkDirection::LEFT)
+        walkDirection -= right;
 
-//     this->Update(GlmToBt(movementDirection), input.isJumping);
-// }
-// void Player::Update(const btVector3 walkDirection, bool isJumping)
-// {
+    if (glm::length(walkDirection) > 0.001f) {
+        walkDirection = glm::normalize(walkDirection);
+    }
 
-//     float speed = 4.0f;
-//     controller->setWalkDirection(walkDirection * speed * FIXED_DELTA_TIME);
+    Update(GlmToBt(walkDirection), input.isJumping);
+}
+void Player::Update(const btVector3 walkDirection, bool isJumping)
+{
 
-//     if (isJumping)
-//     {
-//         if (controller->canJump() && controller->onGround())
-//         {
-//             controller->jump(btVector3(0, controller->getJumpSpeed(), 0));
-//         }
-//     }
-// }
+    const float speed = 4.0f; // TODO: Place this value somewhere global
+
+    controller->setWalkDirection(walkDirection * speed * FIXED_DELTA_TIME);
+
+    if (isJumping) {
+
+        if (!controller->canJump() || !controller->onGround()) return;
+
+        controller->jump(btVector3(0, 4, 0)); // TODO: Place this vector somewhere global
+
+    }
+
+}
